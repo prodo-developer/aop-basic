f266dcb663a09f120655d8ecb8b093fb8b3c2dbe
prodo-developer <wjdals300@gmail.com>
2021-04-14 19:51:20 +0900

    FIX 오토프록싱, @Bean 객체 추가

--- src/main/java/com/aopcode/Application.java
+++ src/main/java/com/aopcode/Application.java
@@ -1,5 +1,7 @@
 package com.aopcode;
 
+import com.aopcode.aspect.Performance;
+import com.aopcode.aspect.UserHistory;
 import com.aopcode.board.Board;
 import com.aopcode.board.BoardRepository;
 import com.aopcode.board.BoardService;
@@ -10,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +22,7 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
+@EnableAspectJAutoProxy //오토 프록싱
 public class Application implements CommandLineRunner {
 
     @Autowired
@@ -41,15 +47,25 @@ public class Application implements CommandLineRunner {
 
     @GetMapping("/boards")
     public List<Board> getBoards() {
-        return boardService.getDataAll();
+        return boardService.getBoards();
     }
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        return userService.getDataAll();
+        return userService.getUsers();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Performance performance() {
+        return new Performance();
+    }
+
+    @Bean
+    public UserHistory userHistory() {
+        return new UserHistory();
     }
 }
